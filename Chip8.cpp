ec7815fc8fd3130bb5de2d30e00124834b06d87a
joe <joe@belgarath>
2014-09-21 22:56:05 -0700

    Fishie.ch8 works\!

--- Chip8.cpp
+++ Chip8.cpp
@@ -42,7 +42,7 @@ void Chip8::run() {
 	bool done = false;
 	SDL_Event e;
 
-	while (!done) {
+		while (!done) {
 		if (SDL_PollEvent(&e)) {
 			if (e.type == SDL_QUIT) {
 				done = true;
@@ -78,25 +78,26 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					printf("RCA 1802");
 				}
 				pc += 2;
-	} break;
+				break;
+	} 
 
 	case 1: {
 				uint16_t newAddr = ((x & 0xF) << 8) | y;
-				printf("NEW PC: %x", newAddr);
-				printf("{%2x %2x }", ram[newAddr], ram[newAddr + 1]);
+				printf("NEW PC: 0x%x ", newAddr);
 				pc = newAddr;
-	} break;
+				break;
+	} 
 
 		// TODO, finish calling subroutine
 	case 2: {
 				uint16_t target = ((x & 0xF) << 8) | y;
 				printf("CALL SUBR AT %x\n", target);
 				// change pc, push addr to stack?
-	} break;
+				break;
+	} 
 
 	case 3: {
-				uint8_t x = x & 0xF;
-				if (V[x] == y) {
+				if (V[x & 0xF] == y) {
 					pc += 4;
 					printf("SKIP NEXT INSTR");
 				}
@@ -104,11 +105,11 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					pc += 2;
 					printf("INCR PC");
 				}
-	} break;
+				break;
+	} 
 
 	case 4: {
-				uint8_t x = x & 0xF;
-				if (V[x] != y) {
+				if (V[x & 0xF] != y) {
 					pc += 4;
 					printf("SKIP NEXT INSTR");
 				}
@@ -116,12 +117,13 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					pc += 2;
 					printf("INCR PC");
 				}
-	} break;
+				break;
+	} 
 
 	case 5: {
-				uint8_t x = x & 0xF;
-				uint8_t y = (y & 0xF0) >> 4;
-				if (V[x] == V[y]) {
+				uint8_t xVal = x & 0xF;
+				uint8_t yVal = (y & 0xF0) >> 4;
+				if (V[xVal] == V[yVal]) {
 					pc += 4;
 					printf("SKIP NEXT INSTR");
 				}
@@ -129,24 +131,28 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					pc += 2;
 					printf("INCR PC");
 				}
-	} break;
+				break;
+	} 
 
 	case 6: {
 				V[x & 0xF] = y;
 				printf("V[%x] = 0x%x", x & 0xF, y);
 				pc += 2;
-	} break;
+				break;
+	} 
 
 	case 7: {
 				V[x & 0xF] += y;
 				printf("V[%x] += 0x%x", x & 0xF, y);
 				pc += 2;
-	} break;
+				break;
+	} 
 
 	case 8: {
 				printf("IMPLEMENT ME!");
 				pc += 2;
-	} break;
+				break;
+	} 
 
 	case 9: {
 				if (V[x & 0xF] != V[y >> 4]) {
@@ -157,23 +163,27 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					pc += 2;
 					printf("INCR PC");
 				}
-	} break;
+				break;
+	} 
 
 	case 0xA: {
 				  I = ((x & 0xF) << 8) | y;
 				  printf("I = 0x%x", I);
 				  pc += 2;
-	} break;
+				  break;
+	} 
 
 	case 0xB: {
 				  pc = (((x & 0xF) << 8) | y) + V[0];
 				  printf("NEW PC: 0x%x", pc);
-	} break;
+				  break;
+	} 
 
 	case 0xC: {
 				  printf("IMPLEMENT ME");
 				  pc += 2;
-	} break;
+				  break;
+	} 
 
 	case 0xD: {
 				  // Draws a sprite at location {Vx, Vy}
@@ -191,14 +201,16 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 					  }
 				  }
 
-
+				  printf("DRAWING SPRITE");
 				  pc += 2;
-	} break;
+				  break;
+	} 
 
 	case 0xE: {
 				  printf("IMPLEMENT ME");
 				  pc += 2;
-	} break;
+				  break;
+	} 
 
 	case 0xF: {
 				  switch (y) {
@@ -206,56 +218,67 @@ void Chip8::interp(uint8_t x, uint8_t y) {
 							  V[x & 0xF] = delayTimer;
 							  printf("V[%x] = 0x%x", x & 0xF, delayTimer);
 							  pc += 2;
-				  } break;
+							  break;
+				  } 
 
 				  case 0xA: {
 								printf("IMPLEMENT ME");
 								pc += 2;
-				  } break;
+								break;
+				  } 
 
 				  case 15: {
 							   delayTimer = V[x & 0xF];
 							   printf("delayTimer = 0x%x", delayTimer);
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  case 18: {
 							   soundTimer = V[x & 0xF];
 							   printf("soundTimer = 0x%x", soundTimer);
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  case 0x1E: {
 								 I += V[x & 0xF];
 								 printf("I += V[0x%x]", x & 0xF);
 								 pc += 2;
-				  } break;
+								 break;
+				  } 
 
 				  case 29: {
 							   printf("IMPLEMENT ME");
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  case 33: {
 							   printf("IMPLEMENT ME");
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  case 55: {
 							   printf("IMPLEMENT ME");
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  case 65: {
 							   printf("IMPLEMENT ME");
 							   pc += 2;
-				  } break;
+							   break;
+				  } 
 
 				  default:
 					  printf("Bad opcode for bottom nybble for case 0xF! Fatal error.");
 					  exit(-1);
 				  }
-	} break;
+
+				  break;
+	} 
 
 	default:
 		printf("UNRECOGNIZED OPCODE. THIS SHOULD NOT HAPPEN. EXITING...");
